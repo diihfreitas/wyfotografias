5c09b99fec60e00b12b4cae8fdd2127071b368f8
Diego Freitas <diegoarturfisio@gmail.com>
2025-01-28 23:04:15 -0300

    novocodigo

--- script.js
+++ script.js
@@ -1,48 +1,13 @@
-let currentIndex = 0;
-const images = document.querySelectorAll('.image-container img');
-const totalImages = images.length;
-const container = document.querySelector('.image-container');
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+const menuNav = document.querySelector('.menu-nav');
 
-document.querySelector('.next').addEventListener('click', () => {
-    if (currentIndex < totalImages - 2) {
-        currentIndex++;
-    } else {
-        currentIndex = 0;
-    }
-    container.style.transform = `translateX(-${currentIndex * 500}px)`;
+menuHamburguer.addEventListener('click', (event) => {
+    menuNav.classList.toggle('active');
+    event.stopPropagation();
 });
 
-document.querySelector('.prev').addEventListener('click', () => {
-    if (currentIndex > 0) {
-        currentIndex--;
-    } else {
-        currentIndex = totalImages - 2;
+document.addEventListener('click', (event) => {
+    if (!menuNav.contains(event.target) && !menuHamburguer.contains(event.target)) {
+        menuNav.classList.remove('active');
     }
-    container.style.transform = `translateX(-${currentIndex * 500}px)`;
-});
-
-
-const link = document.querySelector('.instagram a');
-
-link.innerHTML += link.innerHTML;
-
-let position = 0;
-const speed = 1;
-
-function scrollText() {
-    position -= speed;
-    link.style.transform = `translateX(${position}px)`;
-
-    if (Math.abs(position) >= link.scrollWidth / 2) {
-        position = 0;
-    }
-
-    requestAnimationFrame(scrollText);
-}
-
-scrollText();
-
-document.querySelector('.menu-toggle').addEventListener('click', function () {
-    const menu = document.querySelector('.menu');
-    menu.classList.toggle('active');
 });

21c9517d70e684579b6646885d0c2a12c1acdea4
Diego Freitas <diegoarturfisio@gmail.com>
2025-07-19 12:49:06 -0300

    addcodes

---
+++ willianefotos/fotos/imagens.js
@@ -0,0 +1,23 @@
+import ballet from './ballet.jpg'
+import josilene from './josilene.jpg'
+import flores from './flores.jpg'
+import irmaos from './irmaos.jpg'
+import mariaHeloisa from './maria-heloisa.jpg'
+import red from './red.jpg'
+import sapatinhos from './sapatinhos.jpg'
+import silvania from './silvania.jpg'
+import simone from './simone.jpg'
+import williane from './williane.jpg'
+
+export {
+  ballet,
+  josilene,
+  flores,
+  irmaos,
+  mariaHeloisa,
+  red,
+  sapatinhos,
+  silvania,
+  simone,
+  williane
+} 

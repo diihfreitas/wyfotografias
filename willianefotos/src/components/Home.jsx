12a4c0f1eb4325321532fd7adf1426edda340bd4
Diego Freitas <diegoarturfisio@gmail.com>
2025-07-19 13:15:39 -0300

    addmenu

---
+++ willianefotos/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../../componentscss/Title.css'
+
+const Home = () => {
+  return (
+    <div>
+      <section className='container-title'>
+        <h1 className='title'>Williane Yale</h1>
+        <h2 className='subtitle'>Fotografias</h2>
+      </section>
+      <section className='container-menu'>
+        
+      </section>
+    </div>
+  )
+}
+
+export default Home
